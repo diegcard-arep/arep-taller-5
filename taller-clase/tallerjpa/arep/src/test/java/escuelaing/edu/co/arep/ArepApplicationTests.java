b7d15e70940ef54570f19d7803e4e764d6ca11fb
Diego Cardenas <diegosp778@gmail.com>
2025-09-25 22:07:42 -0500

    taller jpa

---
+++ taller-clase/tallerjpa/arep/src/test/java/escuelaing/edu/co/arep/ArepApplicationTests.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.co.arep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
